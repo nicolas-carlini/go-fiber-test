e701cae2351db71102fc0470c3a94d21ab0de351
nicolas-carlini <nicolascarlini1@gmail.com>
2020-08-08 08:45:24 -0300

    i comment so much

--- book/book.go
+++ book/book.go
@@ -2,20 +2,33 @@ package book
 
 import (
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
 )
 
+//usable book struct
+type Book struct{
+	gorm.Model						//database model
+	Title  string `json: "title"`	//title string 
+	Author string `json: "author"`	//author and playboy by george R.R martin! omg
+	Rating int    `json: "rating"`	//for enthusiastic users 
+}
+
+//getting all books of the data base :D!
 func GetBooks(c *fiber.Ctx){
 	c.Send("all books")
 }
 
+//return a single book 
 func GetBook(c *fiber.Ctx){
 	c.Send("a single book")
 }
 
+//make a new book 
 func NewBook(c *fiber.Ctx){
 	c.Send("add a new book")
 }
 
+//are you sorry
 func DeleteBook(c *fiber.Ctx){
 	c.Send("delete book")
 }
